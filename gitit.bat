4ed513f2333c4571647053e09e99c6aab6fb4c72
robert parker <r.p@grapevine.com.au>
2015-03-24 19:57:31 +1100

    2015Mar21 Copy(54) gitit for copy to git assist

---
+++ gitit.bat
@@ -0,0 +1,9 @@
+cd C:\Users\home\Documents\Developement\HTLM5\veroWeb\veroweb
+copy "other\veroweb - Copy (%1)\home.html" home.html
+copy "other\veroweb - Copy (%1)\vero.js" vero.js
+copy "other\veroweb - Copy (%1)\dorec.js" dorec.js
+copy "other\veroweb - Copy (%1)\js\myScript.js" js\myScript.js
+copy "other\veroweb - Copy (%1)\js\paperGlue.js" js\paperGlue.js
+copy "other\veroweb - Copy (%1)\js\nodeComms.js" js\nodeGlue.js
+dir "other\veroweb - Copy (%1)\js"
+
